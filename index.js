58ef164ac038ff3a86b41c7196f579e5237f9c71
Huzaifa <HuzaifaIslamRakib@gmail.com>
2024-12-08 17:00:09 +0600

    made ready for server deply

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 var cors = require("cors");
 const app = express();
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("EquiSports");
     const users = database.collection("users");
@@ -165,18 +165,8 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
